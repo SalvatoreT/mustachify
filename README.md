5720ac7842bf19887f483a96e828b6c8e297f55c
Salvatore Testa <sal.testa@gmail.com>
2017-01-18 01:37:54 -0800

    Pass through the input as a parameter

--- README.md
+++ README.md
@@ -3,8 +3,16 @@
     Download and unzip shape_predictor_68_face_landmarks.dat from:
     http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 
+I used `bzip2` to unzip the file, but you do whatever works for you.
+
+    bzip2 -d shape_predictor_68_face_landmarks.dat.bz2
+
 Install the Python requirements.
 
     virtualenv -p python3 venv
     source venv/bin/activate
     pip install -r requirements.txt
+
+Once the libraries are all installed, you can run the program on the picture of a face.
+
+    ./mustachify.py -i PICTURE_OF_FACE
--- mustachify.py
+++ mustachify.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 import sys
+import argparse
 import dlib
 from skimage import io
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+def main(face_file):
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
-img = io.imread('sal-face.jpg')
+    img = io.imread(face_file)
 
-dets = detector(img, 1)
+    dets = detector(img, 1)
 
-for k, d in enumerate(dets):
-  print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-      k, d.left(), d.top(), d.right(), d.bottom()))
-  # Get the landmarks/parts for the face in box d.
-  shape = predictor(img, d)
-  print("Part 0: {}, Part 1: {} ...".format(shape.part(0), shape.part(1)))
+    for k, d in enumerate(dets):
+      print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
+          k, d.left(), d.top(), d.right(), d.bottom()))
+      # Get the landmarks/parts for the face in box d.
+      shape = predictor(img, d)
+      print("Part 0: {}, Part 1: {} ...".format(shape.part(0), shape.part(1)))
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    parser = argparse.ArgumentParser(description='This is a demo script by nixCraft.')
+    parser.add_argument('-i','--input', help='Input file name',required=True)
+    args = parser.parse_args()
+    main(args.input)
